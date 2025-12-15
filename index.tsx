c40b04687dacc314c4df69f125b9b240fe4d65a6
whdcks9561-arch <whdcks9561@gmail.com>
2025-12-15 09:55:21 +0900

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -3,14 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 const container = document.getElementById("root");
+if (!container) throw new Error("Root not found");
 
-if (!container) {
-  throw new Error("Root element not found");
-}
-
-const root = createRoot(container);
-
-root.render(
+createRoot(container).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
