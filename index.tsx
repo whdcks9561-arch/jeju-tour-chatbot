eb34b2f2c52b509d2fa34780e0a5436466ec895e
whdcks9561-arch <whdcks9561@gmail.com>
2025-12-15 09:51:22 +0900

    Refactor ReactDOM import and root element handling

--- index.tsx
+++ index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from "./App.tsx";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("Root element not found");
 }
 
-const root = ReactDOM.createRoot(rootElement);
+const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
     <App />
